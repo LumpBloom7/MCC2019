17dfec75402b0968096fbd6f630507f68d18fb54
derrick.timmermans@outlook.com <derrick.timmermans@outlook.com>
2019-10-13 21:55:14 +0800

    Replace elimination solution with a much faster solution

--- Elimination/Elimination.cpp
+++ Elimination/Elimination.cpp
@@ -5,34 +5,29 @@
 #include <fstream>
 
 int main() {
-	// This is a pure brute force method, so not ideal, please share if you have a better algo.
-	size_t maxI = 0;
-	size_t iCount = 0;
 	size_t oCount = 0;
 
-	std::vector<bool> string{}; // Using bool vector since this is a binary string, and we'll save on a bit of RAM
+	std::string string{}; // Using bool vector since this is a binary string, and we'll save on a bit of RAM
+	size_t k = 200366;
 
 	// Using this special input method because the input length could be problematic for some compilers, we're gonna read from file instead.
 	std::ifstream fin("input.txt");
-	while (true) {
-		int tmp = fin.get() - '0';
-		string.emplace_back(static_cast<bool>(tmp));
-		if (fin.eof()) break;
-	}
-	size_t k = 200366;
+	fin >> string;
 
-	// This is basically just running measuring the largest possible contiguous length of 1, on every single element. O(N^2)
-	for (int i = 0; i < string.size(); ++i) {
-		if (string[i] == 1) {
-			iCount = 1;
-			oCount = 0;
-			for (int j = i + 1; j < string.size(); ++j) {
-				if (string[j] == 1) ++iCount;
-				else if (string[j] == 0) ++oCount;
-				if (oCount > k) break;
-			}
-			maxI = iCount > maxI ? iCount : maxI;
-		}
+	size_t longest = 0;
+
+	// Use two pointer method
+	auto leftIt = string.begin(), rightIt = string.begin();
+	while(rightIt < string.end()) {
+		if (*(rightIt++) == '0') ++oCount; // Count every 0 encountered
+
+		while (oCount > k) // If we exceed the 0 limit, we'll need to shift the left pointer
+			if (*(leftIt++) == '0') // If the left pointer happens to be a 0, decrement the 0 counter
+				--oCount;
+
+		// Check the current distance between two pointers minus the counted 0s, this will give us exactly the amount of 1s in the substring
+		longest = (std::distance(leftIt, rightIt-1) + 1 - oCount > longest) ? std::distance(leftIt, rightIt-1) + 1 - oCount : longest;
 	}
-	std::cout << maxI << std::endl;
+
+	std::cout << longest << std::endl;
 }
