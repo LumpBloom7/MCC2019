4644ac870607956b44d7c627ecde0ddcf3e0b1ec
Anatoly Pitikin <xapdkop@xapdkop.space>
2019-10-15 00:02:30 +0300

    Optimize 1st task
    - Calculate min difference in-place (and so remove unnecessary vector and extra loop)

--- Ascending/Ascending.cpp
+++ Ascending/Ascending.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <numeric>
 #include <fstream>
 #include <filesystem>
 #include "../_include/getExePath.h"
 
-int main() {
-	std::filesystem::path inputDir = std::filesystem::path(getExePath()) / "Input" / "Ascending";
-	for (auto& file : std::filesystem::directory_iterator(inputDir)) {
-		std::vector<int> numbers{};
+using namespace std;
 
-		std::ifstream input(file);
-		while (true) {
-			size_t tmp;
-			input >> tmp;
-			numbers.emplace_back(tmp);
-			if (input.eof()) break;
+int main() {
+	filesystem::path inputDir = filesystem::path(getExePath()) / "Input" / "Ascending";
+	for (auto& file : filesystem::directory_iterator(inputDir)) {
+		ifstream input(file);
+
+		size_t previous, minDiff = numeric_limits<size_t>::max();
+		input >> previous;
+
+		while (!input.eof()) {
+			size_t current;
+			input >> current;
+			if (current - previous < minDiff)
+				minDiff = current - previous;
+			previous = current;
 		}
 
-		size_t smallest = std::numeric_limits<size_t>::max(); // This will be initialized to be the biggest number.
-
-		// It's always ascending, so we don't need to sort
-		// We also don't have to compare a value with every other values, we only need to compare adjacent values
-		for (auto i = numbers.begin() + 1; i < numbers.end(); ++i)
-			smallest = (*i - *(i - 1) < smallest ? *i - *(i - 1) : smallest);
-
-		std::cout << smallest << std::endl;
+		cout << minDiff << endl;
 	}
 }
