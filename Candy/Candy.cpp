313733dade8ad420acc58cbb400f85ebb3317a77
Anatoly Pitikin <xapdkop@xapdkop.space>
2019-10-15 02:07:29 +0300

    Wait, that's illegal! heh
    Use different and very optimized idea

--- Candy/Candy.cpp
+++ Candy/Candy.cpp
@@ -1,32 +1,24 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <fstream>
 #include <filesystem>
 #include "../_include/getExePath.h"
 
+using namespace std;
+
 int main() {
-	std::filesystem::path inputDir = std::filesystem::path(getExePath()) / "Input" / "Candy";
-	for (auto& file : std::filesystem::directory_iterator(inputDir)) {
-		std::vector<std::string> shouts{};
-
-		std::ifstream input(file);
-		while (true) {
-			std::string tmp;
+	filesystem::path inputDir = filesystem::path(getExePath()) / "Input" / "Candy";
+	for (auto& file : filesystem::directory_iterator(inputDir)) {		
+		uint64_t currentPosition = 0, currentBit = 0;
+		ifstream input(file);
+		while (!input.eof()) {
+			string tmp;
 			input >> tmp;
-			shouts.emplace_back(tmp);
-			if (input.eof()) break;
+			if (tmp == "odd")
+				currentPosition |= 1ULL << currentBit;
+			++currentBit;
 		}
 
-		size_t currentPosition = 1; // We end at one.
-
-		// We are simply gonna reverse the process
-		for (auto i = shouts.rbegin(); i < shouts.rend(); ++i) {
-			if (*i == "odd") // If odd is shouted, our current position is guaranteed to double
-				currentPosition += currentPosition;
-			else if (*i == "even" && currentPosition > 0) // If even is shouted and we aren't in the first spot, the people in front of us is doubled.
-				currentPosition += currentPosition - 1;
-		}
-		std::cout << currentPosition << std::endl;
+		cout << ++currentPosition << endl;
 	}
 }
