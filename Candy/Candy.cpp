bda0ade1f96510d637d42e11dd66c26c48d862d8
derrick.timmermans@outlook.com <derrick.timmermans@outlook.com>
2019-10-15 12:49:46 +0800

    Add link to PR
    For people who wants to understand what's going on
    Probably, will improve this in the future with readmes in each project dir

--- Candy/Candy.cpp
+++ Candy/Candy.cpp
@@ -6,9 +6,11 @@
 
 using namespace std;
 
+// Read a brief explaination at https://github.com/LumpBloom7/MCC2019/pull/2
+
 int main() {
 	filesystem::path inputDir = filesystem::path(getExePath()) / "Input" / "Candy";
-	for (auto& file : filesystem::directory_iterator(inputDir)) {		
+	for (auto& file : filesystem::directory_iterator(inputDir)) {
 		uint64_t currentPosition = 0, currentBit = 0;
 		ifstream input(file);
 		while (!input.eof()) {
